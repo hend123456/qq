b940e16a0d2364bb5015ce366e54c247be78b639
hend <hblhhnd@163.com>
2020-02-14 20:54:54 +0800

    update2

--- src/main/java/qq/App.java
+++ src/main/java/qq/App.java
@@ -4,10 +4,11 @@ package qq;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello World2!" );
     }
 }
